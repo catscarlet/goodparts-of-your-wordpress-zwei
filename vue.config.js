e3779ce8dffe9a131c638a6eb59cba1a116fba1f
catscarlet <i@catscarlet.com>
2022-02-04 20:31:00 +0800

    Better devServer example.

--- vue.config.js
+++ vue.config.js
@@ -8,15 +8,23 @@ let config = {
 
 //If you want to `yarn serve` over HTTPS. Try uncomment below.
 //For more information, read `https://webpack.js.org/configuration/dev-server/#devserverhttps` and `https://github.com/FiloSottile/mkcert`
-/*
+
 config.devServer = {
+    proxy: {
+        '/api': {
+            target: 'http://127.0.0.1/goodparts-of-your-wordpress-zwei/api/',
+            pathRewrite: {'^/api': ''},
+        },
+    },
+    port: 8080,
+    /*
     https: {
         cert: fs.readFileSync('./test_cert/localhost.cert'),
         key: fs.readFileSync('./test_cert/localhost.key'),
         ca: fs.readFileSync('./test_cert/localhostCA.pem'),
     },
-    port: 8443,
+    port: 8443, // devServer can only listen either http or https at a time.
+    */
 };
-*/
 
 module.exports = config;
